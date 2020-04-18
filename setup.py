86ebeb183e9d15c73b80967af7eef0f1bf097ab9
Bharat Sinha <bharat.sinha.2307@gmail.com>
2020-04-17 18:26:23 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="bharat23", # Replace with your own username
     version="0.0.1",
     author="Bharat Sinha",
-    author_email="bsinha@ancestry.com",
-    description="A small example package",
+    author_email="bharat.sinha.2307@gmail.com",
+    description="A plug-n-play package to start using new relic APIs for data gathering",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Bharat23/newrelic-api-parser",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
