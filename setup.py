378c4447963d2cbff14b99103efe530628538cce
Bharat Sinha <bharat.sinha.2307@gmail.com>
2020-05-18 19:31:42 -0600

    update setup.py for long description

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 import setuptools
 
+def read(fname):
+    """ Return file content. """
+    with open(fname) as f:
+        content = f.read()
+
+    return content
+
+description = "A plug-n-play package to start using new relic APIs for data gathering"
+try:
+    long_description = read('README.MD')
+except IOError:
+    long_description = description
+
 setuptools.setup(
     name="newrelic-api-parser",
     package=['newrelic-api-parser'],
     version="0.0.6",
     author="Bharat Sinha",
     author_email="bharat.sinha.2307@gmail.com",
-    description="A plug-n-play package to start using new relic APIs for data gathering",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/Bharat23/newrelic-api-parser",
     packages=setuptools.find_packages(),
     license='MIT',
