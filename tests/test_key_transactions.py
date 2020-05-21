d2cf1d17306d784e13056c1d4345caa03deed28f
Bharat Sinha <bharat.sinha.2307@gmail.com>
2020-05-21 15:49:37 -0600

    add key transaction

---
+++ tests/test_key_transactions.py
@@ -0,0 +1,15 @@
+import unittest
+import os
+
+from NewRelic import Config
+from NewRelic.REST.KeyTransactions import KeyTransactions
+
+class KeyTransactionsTest(unittest.TestCase):
+
+    def test_list(self):
+        API_KEY = os.environ.get('NEWRELIC_API_KEY')
+        key_transactions = KeyTransactions(API_KEY=API_KEY)
+        self.assertNotEqual(key_transactions.get_list(), None) 
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
